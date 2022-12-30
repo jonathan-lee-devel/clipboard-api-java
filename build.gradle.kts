283e38539561271a7c0c23013b79c6f0719d9829
Jonathan Lee <jonathan.lee.devel@gmail.com>
2022-12-30 03:32:20 +0000

    Added sonar to the build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ version = "1.0-SNAPSHOT"
 plugins {
     application
     id("jacoco-report-aggregation")
+    id("org.sonarqube") version "3.5.0.2730"
     id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
 }
@@ -18,4 +19,12 @@ dependencies {
 
 tasks.check {
     dependsOn(tasks.named<JacocoReport>("testCodeCoverageReport"))
+}
+
+sonar {
+    properties {
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "jonathan-lee-devel")
+        property("sonar.projectKey", "jonathan-lee-devel_clipboard-api")
+    }
 }
