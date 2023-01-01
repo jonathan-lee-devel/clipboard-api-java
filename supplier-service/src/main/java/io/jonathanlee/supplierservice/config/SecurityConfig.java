a009626772695a0fe4beb661e2f662df7f6c858f
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-01-01 15:25:39 +0000

    Added supplier-service Security Config

---
+++ supplier-service/src/main/java/io/jonathanlee/supplierservice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package io.jonathanlee.supplierservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+    return http
+        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).csrf().disable()
+        .formLogin(Customizer.withDefaults())
+        .build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}
